3908edfa1cf918c090f9292d3c97f51be5d96f24
Priyanuj Bora <while.alive.program@gmail.com>
2023-03-08 10:10:27 +0530

    Simple C program

---
+++ Lex/EXPs/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int fact(int);
+
+int main(void)
+{
+	int num;
+
+	printf("enter a number: ");
+	scanf("%d", &num);
+
+	printf("%d! = %d\n", num, fact(num));
+
+	return 0;
+}
+
+int fact(int num)
+{
+	if(num==0)
+		return 1;
+	return (num*fact(num-1));
+}
