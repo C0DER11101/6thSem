21e1afe067b887b8a8042c0f5b4954f8710baba8
Priyanuj Bora <while.alive.program@gmail.com>
2023-03-30 17:02:29 +0530

    New C file created by Replace.l after replacing each float by a double

---
+++ Lex/EXPs/main3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+double add(double, double);
+
+int main(void)
+{
+	double a, b;
+
+	double c, d;
+
+	add(a, b);
+
+	return 0;
+}
+
+double add(double a, double b)
+{
+	return 9;
+}
