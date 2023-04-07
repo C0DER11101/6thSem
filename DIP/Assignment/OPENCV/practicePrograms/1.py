a6ad41b1b4a15f1701f130b1099675e93131072e
Priyanuj Bora <while.alive.program@gmail.com>
2023-04-07 21:22:43 +0530

    read, write and show image using OpenCV

--- DIP/Assignment/OPENCV/practicePrograms/1.py
+++ DIP/Assignment/OPENCV/practicePrograms/1.py
@@ -3,7 +3,7 @@ import cv2
 img=cv2.imread('HappyFish.jpg', 0);
 
 cv2.imshow('HappyFish', img);
-key=cv2.waitKey(5000); # stay for 5s.
+key=cv2.waitKey(5000) & 0xFF # stay for 5s.
 
 if(key==27):
     cv2.destroyAllWindows();
