55f12c47109f42dacabd588dfd2ce62cc0028f03
Priyanuj Bora <while.alive.program@gmail.com>
2023-04-08 22:31:02 +0530

    colors and color detection

--- DIP/Assignment/OPENCV/practicePrograms/7.py
+++ DIP/Assignment/OPENCV/practicePrograms/7.py
@@ -18,7 +18,7 @@ while(True):
     lower_blue=np.array([90, 50, 50]); # light blue
     upper_blue=np.array([130, 255, 255]); # dark blue
 
-    mask=cv2.inRange(hsv, lower_blue, upper_blue); # returns a new image/a mask containing those pixels in the given range between lower_blue and upper_blue that need to be removed.
+    mask=cv2.inRange(hsv, lower_blue, upper_blue); # returns a new image/a mask those pixels in the given range between lower_blue and upper_blue that need to be displayed.
 
     result=cv2.bitwise_and(frame, frame, mask=mask);
     """
@@ -32,6 +32,7 @@ while(True):
     """
 
     cv2.imshow('Detect', result);
+    cv2.imshow('mask', mask);
 
     if(cv2.waitKey(1)==ord('q')):
         break;
