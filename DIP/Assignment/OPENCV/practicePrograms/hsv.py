32b69f8cbfb722bec88fdc61ac90eaabbf2f7a62
Priyanuj Bora <while.alive.program@gmail.com>
2023-04-12 10:43:53 +0530

    Detecting blue using trackbars

--- DIP/Assignment/OPENCV/practicePrograms/hsv.py
+++ DIP/Assignment/OPENCV/practicePrograms/hsv.py
@@ -1,19 +1,45 @@
 import cv2
 import numpy as np
 
-cv2.namedWindow("Tracking");
+def nothing(x):
+    pass;
 
-cv2.
+cv2.namedWindow('Tracking');
+
+cap=cv2.VideoCapture(0);
+
+#Lower Hue, Saturation and Value
+cv2.createTrackbar('LH', 'Tracking', 0, 255, nothing);
+cv2.createTrackbar('LS', 'Tracking', 0, 255, nothing);
+cv2.createTrackbar('LV', 'Tracking', 0, 255, nothing);
+
+#Upper Hue, Saturation and Value
+cv2.createTrackbar('UH', 'Tracking', 255, 255, nothing);
+cv2.createTrackbar('US', 'Tracking', 255, 255, nothing);
+cv2.createTrackbar('UV', 'Tracking', 255, 255, nothing);
+
 
 while True:
-    frame=cv2.imread('smarties.png');
+    _, frame=cap.read();
 
     hsv=cv2.cvtColor(frame, cv2.COLOR_BGR2HSV);
 
-    lower_blue=np.array([110, 50, 50]);
-    upper_blue=np.array([130, 255, 255]);
 
-    mask=cv2.inRange(hsv, lower_blue, upper_blue);
+    l_h=cv2.getTrackbarPos('LH', 'Tracking');
+    l_s=cv2.getTrackbarPos('LS', 'Tracking');
+    l_v=cv2.getTrackbarPos('LV', 'Tracking');
+
+    u_h=cv2.getTrackbarPos('UH', 'Tracking');
+    u_s=cv2.getTrackbarPos('US', 'Tracking');
+    u_v=cv2.getTrackbarPos('UV', 'Tracking');
+
+    lower_blue=np.array([l_h, l_s, l_v]); # light blue
+    upper_blue=np.array([u_h, u_s, u_v]); # dark blue
+
+    #lower_blue=np.array([110, 50, 50]); # light blue
+    #upper_blue=np.array([130, 255, 255]); # dark blue
+
+    mask=cv2.inRange(hsv, lower_blue, upper_blue); # thresholding the hsv image to get the blue color
 
     result=cv2.bitwise_and(frame, frame, mask=mask);
 
@@ -25,4 +51,5 @@ while True:
         break;
 
 
+cap.release();
 cv2.destroyAllWindows();
